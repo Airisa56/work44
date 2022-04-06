65b220018cd69dad355c1a6b30168c97cfdd8334
Airisa <airisa56>
2022-04-06 20:49:46 +0300

    коммит 4

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,40 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Задание 1
+        int a = 1;
+        while (a <= 10) {
+            System.out.print(a + " ");
+            a++;
+        }
+        System.out.println();
+        for (a = 10; a > 0; a--) {
+            System.out.print(a + " ");
+        }
+        {
+            System.out.println();
+
+        }
+
+
+        // Задание 2
+        int p = 6;
+        while (p <= 27) {
+            System.out.println(" Сегодня пятница " + p + " число. Необходимо подготовить отчет");
+            p = p + 7;
+        }
+
+
+        // Задание 3
+         int x= 1896 ;
+        while ( x <= 2054  ) {
+            System.out.println( x);
+            x = x+ 79;
+        }
     }
-}
+     }
+
+
+
+
